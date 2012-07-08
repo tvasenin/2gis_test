ea4d9f78fc0135751314cf711a0d2044148b90b4
Timofey Vasenin <timofey.vasenin@gmail.com>
2012-07-09 03:38:02 +0700

    Minor change in parsing logic

--- 2gis_test.cpp
+++ 2gis_test.cpp
@@ -78,30 +78,30 @@ int main(int argc, char* argv[])
             return 0;
         }
         else if (0 == std::strcmp(argv[loop], "-f")) {
-            if (argc <= ++loop) {
+            if (argc <= loop + 1) {
                 usage(std::cerr);
                 return 1;
             }
             else {
-                ifname = argv[loop];
+                ifname = argv[++loop];
             }
         }
         else if (0 == std::strcmp(argv[loop], "-m")) {
-            if (argc <= ++loop) {
+            if (argc <= loop + 1) {
                 usage(std::cerr);
                 return 1;
             }
             else {
-                mode = argv[loop];
+                mode = argv[++loop];
             }
         }
         else if (0 == std::strcmp(argv[loop], "-v")) {
-            if (argc <= ++loop) {
+            if (argc <= loop + 1) {
                 usage(std::cerr);
                 return 1;
             }
             else {
-                search_str = argv[loop];
+                search_str = argv[++loop];
             }
         }
         else {
